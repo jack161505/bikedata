91734cb35ab243e461860563405f1d45e2afbc7c
jackli <lishanxi161@163.com>
2022-01-25 17:22:11 +0800

    bikedata commit

---
+++ bikedata/src/main/java/com/jack/bikedata/BikedataApplication.java
@@ -0,0 +1,13 @@
+package com.jack.bikedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikedataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikedataApplication.class, args);
+	}
+
+}
---
+++ bikedata/src/main/java/com/jack/bikedata/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.jack.bikedata.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("/list")
+    public String getList()
+    {
+        String list="test";
+        return list;
+    }
+}
